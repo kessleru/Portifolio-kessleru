6c91691bf897baabffcc5bdb4e0f7f0265797c60
kessleru <otaviokesslerustra@hotmail.com>
2026-02-18 21:27:15 -0300

    fix: commit de 1 linha

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -9,7 +9,7 @@ const Modal: React.FC<{
   if (!isOpen) return null;
 
   return createPortal(
-    <div className="absolute right-4 top-20 z-50 animate-in fade-in zoom-in-95 slide-in-from-top-2 duration-300 ease-out sm:right-8">
+    <div className="absolute right-4 top-27 z-50 animate-in fade-in zoom-in-95 slide-in-from-top-2 duration-300 ease-out sm:right-8">
       <Card fileName="Modal.tsx" className="w-[min(10rem,calc(100vw-2rem))]">
         {children}
       </Card>
